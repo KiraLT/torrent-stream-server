eb6ba74567fc598e8d58a4cd161c8adcac365249
Luke <lukasmikuckis@gmail.com>
2020-08-02 17:28:59 +0300

    Demo build moved to git (because of docker)

---
+++ demo/build/precache-manifest.37434d1727aaf674c461ae2f5e3b4560.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f7a7729e183b3ec85b6d59e1911eb8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28b4ccfa11ca98548dd5",
+    "url": "/static/js/2.3de99557.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3de99557.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a64d01e85f291620a706",
+    "url": "/static/js/main.3791c586.chunk.js"
+  },
+  {
+    "revision": "fa0af807e30880911a9d",
+    "url": "/static/js/runtime-main.28faca3d.js"
+  }
+]);
