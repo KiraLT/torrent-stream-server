443ee05b087cbd8e8c4ce44f5d69852fd8427c88
Luke <KiraLT@users.noreply.github.com>
2020-11-07 19:07:54 +0200

    Fixes

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -5,7 +5,3 @@ import { serveCommand } from './serve'
 export const buildCli = (args?: string[]) => {
     return yargs(args || process.argv.slice(2)).command(serveCommand).demandCommand().help()
 }
-
-if (!module.parent) {
-    buildCli().argv
-}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
+#!/usr/bin/env node
+
 export * from './app'
 export * from './config'
 export * from './api'
 export * from './models'
 export * from './services'
 export * from './cli'
+
+import { buildCli } from './cli'
+
+if (!module.parent) {
+    buildCli().argv
+}
