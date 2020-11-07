6f06bafc41e8d402c696009ce536f5c7143bb7a2
Luke <KiraLT@users.noreply.github.com>
2020-11-07 16:29:31 +0200

    gitpod improvements

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,9 +1,17 @@
 import { Configuration } from './helpers/client'
 
-export const apiDomain =
-    process.env.NODE_ENV === 'production'
-        ? `${window.location.protocol}//${window.location.host}`
-        : 'http://127.0.0.1:3000'
+export const apiDomain = (() => {
+    if (process.env.NODE_ENV === 'production') {
+        return`${window.location.protocol}//${window.location.host}`
+    }
+    if (window.location.host.includes('gitpod')) {
+        return `${window.location.protocol}//${[
+            3000,
+            ...window.location.host.split('-').slice(1)].join('-')}`
+    }
+    return 'http://127.0.0.1:3000'
+})()
+
 
 export function getApiConfig(options?: { bearer?: string }): Configuration {
     const { bearer } = options || {}
